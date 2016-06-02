e583e8877ab4b81d5a481148767753d1f74ff59e
toerry <patrikdue_2@hotmail.com>
2016-06-02 12:20:02 +0200

    added a prediction according to the neighborhood structure

--- produceSubmission.py
+++ produceSubmission.py
@@ -8,11 +8,66 @@ from sklearn.cluster import KMeans
 import sys
 import random
 import os.path
+from math import sqrt,pi,exp
 
 BEST_K = 5
-NMB_OF_TRAINING_ITERATIONS = 10000000
+NMB_OF_TRAINING_ITERATIONS = 100000
 LEARNING_RATE = 0.001
+SIGMA = 0.2
 SUBMISSION_FILENAME = "mySubmission.csv"
+
+
+def getUserDistance(userA, userB):
+    return np.linalg.norm(userA-userB, ord=2)
+
+def dnorm(X,mu=0,sigma=1.5):
+    """
+    Named after the dnorm function from programming language R.
+    Density generation for the normal distribution with mean = mu and standard deviation = sigma.
+    Input: 
+     - *x* : vector or integer of 'x-axis' values
+     - *mu* [default = 0]
+     - *sigma* [defaul = 1.5]
+    Output: 
+     - float or list with floats containing the calculated values
+    """
+    dnorm_list = []  
+    constant = 1.0/sqrt(2.0*pi*sigma**2)      # Calc once
+    denominator = (2.0*sigma**2)          # Calc once
+    if type(X) == list:               # For a list of int/floats
+        for x in X:
+            value = constant*exp(-((x-mu)**2)/denominator)
+            dnorm_list.append(value) 
+        return dnorm_list    
+    elif type(X) == int or float:         # For a single int/float
+        value = constant*exp(-((X-mu)**2)/denominator)
+        return value
+
+    else:
+        print "The first argument (X) must be a list with integers/floats or a single integer/float.\n"
+        sys.exit()    
+
+def normalized_dnorm(X,mu=0,sigma=1.5):
+    """
+    Generate normalized values based on the maximum of the bell-shaped normal distribution. 
+    It uses the self defined dnorm function to calculate these values.
+    Input: 
+     - *x* : vector or integer of 'x-axis' values
+     - *mu* [default = 0]
+     - *sigma* [defaul = 1.5]*
+    Output: 
+     - float or list with floats containing the calculated values
+    """
+    unnormalized = dnorm(X,mu,sigma)      # Do unnormalized dnorm
+    max_ = 1.0/sqrt(2.0*pi*sigma**2)      # Max value at x = 0
+
+    norm_dnorm_list = []
+    if type(X) == list:               # For a list of int/floats
+        for x in unnormalized:
+            norm_dnorm_list.append(x/max_)
+        
+    elif type(X) == int or float:         # For a single int/float
+        return unnormalized/max_
 
 def irmse(predicted_matrix,validation_ids):
     num_of_items = len(validation_ids)
@@ -123,6 +178,8 @@ else:
     validation_ids = np.load("validation_ids.npy")
     test_ids = np.load("test_ids.npy")
     print "Data loaded from files"
+    #plt.hist(training_ids[:,0,1], 10000)
+    #plt.show()
 
 
 rand_ids = np.random.choice(range(0,len(training_ids)), size=NMB_OF_TRAINING_ITERATIONS)
@@ -161,6 +218,7 @@ mses = irmse(prediction_matrix,validation_ids)
 print "The overall RMSE prediction error for selected " + str(BEST_K) + " optimized singular values and reg_term " + str(0) +" is: " + str(mses)
 
 
+
 #find the indices of the items we have to predict and store them
 item_predict_index = []
 user_predict_index = []
@@ -181,10 +239,44 @@ for k in range(0,len(test_ids)):
 
 prediction_indices = zip(item_predict_index,user_predict_index)
 
+############################
+#do NN stuff
+############################
 #find the predictions
+counter = 1
 predictions = []
 for prediction_index in prediction_indices:
-    predictions.append(prediction_matrix[prediction_index[0],prediction_index[1]])
+    userIdx = prediction_index[0]
+    movieIdx = prediction_index[1]
+    
+    currentUser = U[userIdx,:]
+    compare_ids = np.where(training_ids[:,0,1] == movieIdx)[0]
+    
+    weights = np.zeros(len(compare_ids))
+    for i in range(0,len(compare_ids)):
+        idx = training_ids[compare_ids[i],0,0]
+
+        compareUser = U[idx,:]
+        d = getUserDistance(currentUser, compareUser)
+        #print d
+        weights[i] = dnorm(d, mu=0, sigma=SIGMA)
+        
+
+    #normalize weights
+    weights = weights / np.sum(weights)
+    #print weights
+
+    final_rating = 0
+    for i in range(0,len(compare_ids)):
+        rating = training_ids[compare_ids[i],0,2]
+        final_rating += weights[i]*rating
+
+    predictions.append(min(5,max(1,final_rating)))
+    counter += 1
+    if (np.mod(counter, 1000) == 0):
+        print counter
+
+
 
 #generate prediction file
 predictions_file = open("submissions/" + SUBMISSION_FILENAME, "wb")
